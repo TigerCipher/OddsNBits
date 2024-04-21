6cf34707ca9c74b5dd5d752e5d8a0cbe027bd2ab
TigerCipher <67520209+TigerCipher@users.noreply.github.com>
2024-04-20 23:55:32 -0500

    more design work - featured blog on home page

---
+++ OddsNBits/wwwroot/js/common.js
@@ -0,0 +1,14 @@
+﻿document.addEventListener("DOMContentLoaded", _ => {
+    const topnav = document.getElementsByClassName('oddsnav')[0];
+    if (topnav) {
+        window.onscroll = () => {
+            if (window.scrollY > 50) {
+                // add classes scrollednav py-0
+                topnav.classList.add('scrollednav', 'py-0');
+            } else {
+                // remove classes scrollednav py-0
+                topnav.classList.remove('scrollednav', 'py-0');
+            }
+        }
+    }
+});
