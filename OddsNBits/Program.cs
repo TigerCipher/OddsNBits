bfa74c52fcc2eb664076fc9c9332014665b651d0
TigerCipher <67520209+TigerCipher@users.noreply.github.com>
2024-05-13 20:27:23 -0500

    policies and theme credit

--- OddsNBits/Program.cs
+++ OddsNBits/Program.cs
@@ -53,7 +53,7 @@ builder.Services.AddDbContextFactory<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddIdentityCore<ApplicationUser>(/*options => options.SignIn.RequireConfirmedAccount = true*/)
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddSignInManager()
